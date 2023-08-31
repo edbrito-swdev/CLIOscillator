b6dc512a0cbe9bb484b6bd7ba219427682e881b5
Eduardo Brito <edbrito.swdev@gmail.com>
2023-08-31 02:23:19 +0100

    Minimal CLI example for AudioKit.

--- CLIOscillator/main.swift
+++ CLIOscillator/main.swift
@@ -5,7 +5,19 @@
 //  Created by Eduardo Brito on 31/08/2023.
 //
 
+import AudioKit
 import Foundation
 
-print("Hello, World!")
+let osc = PlaygroundOscillator()
 
+let engine = AudioEngine()
+engine.output = osc
+try! engine.start()
+
+osc.play()
+
+while true {
+    osc.frequency = Float.random(in: 200 ... 800)
+    osc.amplitude = Float.random(in: 0.0 ... 0.3)
+    usleep(100_000)
+}
